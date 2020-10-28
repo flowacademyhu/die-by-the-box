328666d2e63cc6f098e8ea8f7310a9ee6ce03f83
nomansnick <marapatgab@gmail.com>
2020-10-28 08:48:55 +0100

    spawnfix

--- boxes.js
+++ boxes.js
@@ -2,10 +2,10 @@ const map = require("./map");
 
 const spawnBoxes = (boxNum, szelesseg) => {
     let boxTomb = []
-    let boxdarab = { posx: 0, posy: 1,}
+    let boxdarab = { posx: 0, posy: 0,}
     let box_x = 0; 
     for (let i = 0; i < boxNum; i++) {
-        boxdarab = { posx: 0, posy: 1,}
+        boxdarab = { posx: 0, posy: 0,}
         box_x = Math.floor(Math.random() * szelesseg);
         boxdarab.posx = (box_x);
         boxTomb.push(boxdarab);
