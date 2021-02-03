53c1f97fe9fd5dbc31114e975e876a5b36ab9d67
Roland Németh <info@nemethroland.hu>
2021-02-03 09:53:02 +0100

    Format score player function better

--- boxes.js
+++ boxes.js
@@ -104,13 +104,15 @@ const ScoreTorlesScore = (scoreTomb, jatekos, dobozok) => {
 
 const ScorePlayer = (scoreTomb, jatekos) => {
     let talalatok = 0;
+    
     for (let l = 0; l < scoreTomb.length; l++) {
         talalatok = 0;
         if (scoreTomb[l].posy === jatekos.posy && scoreTomb[l].posx === jatekos.posx) {
         jatekos.points++
         }
-}
-     return jatekos.points
+    }
+    
+    return jatekos.points
 }
 
 
